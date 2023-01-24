540cbf6a2312d58afa56fc99f722ae141badd8d2
RomnSh <110737474+RomnSh@users.noreply.github.com>
2023-01-24 23:27:26 +0200

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,15 +8,15 @@ const NavBar = () => {
         
 <nav className="bg-slate-400 border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
   <div className="container flex flex-wrap items-start justify-between mx-auto">
-  <a href="/#" className="flex items-center">
+  <Link to={process.env.PUBLIC_URL + '/'} className="flex items-center">
     <img src={img} className="h-12 mr-5 sm:h-13" alt="Flowbite Logo" />
       <span className="self-center text-xl font-semibold  text-white">Crypto</span>
-  </a>
+  </Link>
      <div className="items-start justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-search">
      
       <ul className="flex flex-row p-4 mx-5 border border-gray-100 rounded-lg bg-gray-50 ">
         <li>
-          <Link to="/"  className="block mx-5 text-gray-700 rounded"> Home</Link>
+          <Link to={process.env.PUBLIC_URL + '/'}  className="block mx-5 text-gray-700 rounded"> Home</Link>
         </li>
         <li>
           <Link to="/About" className="block mx-5 text-gray-700 rounded">About</Link>
